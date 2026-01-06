c15baff7630e70249e95b181f354793e7380fd51
guptaashwini-er <guptaashwini9044@gmail.com>
2026-01-06 20:29:51 +0530

    Update ProductBUtton.js

--- src/component/product/ProductBUtton.js
+++ src/component/product/ProductBUtton.js
@@ -15,7 +15,7 @@ function Product() {
         <ul className="nav-links">
         <li><a href="https://www.instagram.com/tech__ashwin?igsh=MXZ3aWJzdzdhbWVibA%3D%3D">Instagram</a></li>
         <li><a href="https://www.facebook.com/techashwin">Facebook</a></li><br/>
-        <li><a href="https://www.snapchat.com/add/techashwin">Snapchat</a></li>
+        <li><a href="https://t.me/tech_ashwin">Telegram</a></li>
          <li><a href="http://www.youtube.com/@tech.ashwini">Youtube</a></li>
       </ul>
         </div>
