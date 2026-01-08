ef6fa45b57f2e11a340e36e428847d5194ba9653
guptaashwini-er <tanish0007yt@gmail.com>
2026-01-09 00:05:54 +0530

    Route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as BrauserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Hero from './component/Hero';
 import Instagram from './profile/Instagram';
@@ -8,13 +8,13 @@ import Instagram from './profile/Instagram';
 function Rander() {
   return ( 
 
-    <BrauserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Hero/>}/>  
         <Route path='/about' element={<h1>About Page</h1>}/>
         <Route path='/profile' element={<Instagram/>}/>
       </Routes>
-    </BrauserRouter>
+    </HashRouter>
   );
 }
 
