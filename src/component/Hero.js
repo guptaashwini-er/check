d233bc032dc40f06ed5653cd6f0652162534175c
guptaashwini-er <tanish0007yt@gmail.com>
2026-01-09 14:39:46 +0530

    new

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,19 +1,19 @@
 import Instagram from "../profile/Instagram";
 import Navbar from "./NavBar";
 import Product from "./product/ProductBUtton";
-import ProfileMenu from "./Profile";
+// import ProfileMenu from "./Profile";
 import Radhe from "../radhe/Radhe";
 
 function Hero() {
     return ( 
         <>
         <Navbar/>
-
-
-        <ProfileMenu/>
-        <Product/>
-        <Instagram/>
+        <Instagram/>   
+        <br/>    
+             
         <Radhe/>
+
+         <Product/> 
         
         
         </>
